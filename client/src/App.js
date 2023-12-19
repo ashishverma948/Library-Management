5288cb80bb02e828ad86eeee3de4fa1876293042
Ashish Kumar Verma <136198661+ashishverma948@users.noreply.github.com>
2023-12-20 03:53:43 +0530

    antd and add button

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 
-
+import { Button, Flex } from 'antd';
 function App() {
   return (
     <div className="App">
-      <h1>Library-Management<h1/>
+      <h1>Library-Management</h1>
+      <Space wrap>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+    </Space>
     </div>
   );
 }
