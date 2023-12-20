6e93db5fe34eec44081d71e0c1a83e5a3b159903
Himanshuagrawal2 <98666152+Himanshuagrawal2@users.noreply.github.com>
2023-12-21 00:18:25 +0530

    login page with submit button

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,47 @@
-import React from 'react'
-
+import React from 'react';
+import {Button, Form} from "antd";
+import Button from '../../components/Button';
+import { Link } from 'react-router-dom';
 function Login() {
+  const onFinish = (values) =>{
+    console.log("Success",values);
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <div className='h-screen bg-primary flex item-center justify-center'>
+      <div className='authentication-form bg-white p-2'>
+   <h1 className="text-secondary text-2xl">
+        Libray Login
+        </h1>
+        <hr />
+<Form layout="vertical"
+onFinish={onFinish}
+ className='mt-1 p-3'>
+<Form.Item
+label="Email"
+name="email">
+  <Input type="email" placeholder="Email" />
+</Form.Item>
 
+<Form.Item
+label="Password"
+name="password"
+>
+  <Input type="password" placeholder="Password" />
+
+</Form.Item>
+<Button title="Login" 
+type ="submit" color="secondary" varient="outlined"/>
+<div className="text-center mt-2 flex flex-col gap-1">
+  <Link to="/register"
+  className="text-primary text-sm underline">Don't Have an account ? click Here to register</Link>
+</div>
+
+</Form>
+
+
+
+      </div>
+    </div>
+  
+  )}
 export default Login
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import {Button, Form} from "antd";
+import Button from '../../components/Button';
+import { Link } from 'react-router-dom';
 function Register() {
+  const onFinish = (values) =>{
+    console.log("Success",values);
+  };
   return (
     <div className='h-screen bg-primary flex item-center justify-center'>
       <div className='authentication-form bg-white p-2'>
+   <h1 className="text-secondary text-2xl">
+        Libray Register
+        </h1>
+        <hr />
 <Form layout="vertical"
 onFinish={onFinish}
- >
+ className='mt-1 p-3'>
 <Form.Item
-label ="Email"
+label ="Name"
 name="email"
 >
   <input type="text" placeholder='Name'/>
 </Form.Item>
 <Form.Item
-
-label="Name"
+label="Email"
 name="email">
   <Input type="email" placeholder="Email" />
 </Form.Item>
@@ -27,15 +35,21 @@ name="password"
   <Input type="password" placeholder="Password" />
 
 </Form.Item>
+<div className="text-center mt-2 flex flex-col gap-1">
+
 <Button title="Register" 
 type ="submit" color="secondary" varient="outlined"/>
+
+  <Link to="/login"
+  className="text-primary text-sm underline">Already Have an account ? click Here to Login</Link>
+</div>
+
 </Form>
 
 
 
       </div>
     </div>
-  )
-}
-
+  
+  )}
 export default Register
