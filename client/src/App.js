c3bdb6204dfa6bcf8e538e63185dd6d5b97bc0ad
Ashish Kumar Verma <136198661+ashishverma948@users.noreply.github.com>
2023-12-21 12:14:22 +0530

    stylesheet file add

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
+// import browse router redux toolkit
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Home from './pages/Login';
 import Home from './pages/Register';
 
+// import css
+import './stylesheets/alignments.css';
+import './stylesheets/theme.css';
+import './stylesheets/sizes.css';
+import './stylesheets/custom-components.css';
+import './stylesheets/form-elements.css';
+
+
+
+
 import { Button, Divider, Flex } from 'antd';
+
 function App() {
   return (
    <div>
@@ -13,9 +26,10 @@ function App() {
      <Route path="/login" element={<Login />} />
      <Route path="/register" element={<Register />} />
      </Routes>
+     
       </BrowserRouter>
      
-
+ 
    </div>
   );
 }
