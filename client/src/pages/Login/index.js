698a129522108114d7e993154ead26b359c0a61f
priyanshupg <gpriyanshu752@gmail.com>
2023-12-22 23:42:45 +0530

    login,register,home pages changes

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,23 +1,18 @@
-import React, { useEffect } from 'react';
-import { Button, Form, message } from "antd";
-import Button from '../../components/Button';
-import { Link, useNavigate } from 'react-router-dom';
-import { LoginUser } from '../../apicalls/users';
-import { response } from 'express';
-
-import { useDispatch } from 'react-redux';
-import { HideLoading, ShowLoading } from '../../redux/loadersSlice';
+import React, { useEffect } from "react";
+import { Form, message } from "antd";
+import Button from "../../components/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { LoginUser } from "../../apicalls/users";
+import { useDispatch } from "react-redux";
+import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
 
 function Login() {
   const navigate = useNavigate();
-
   const dispatch = useDispatch();
-
   const onFinish = async (values) => {
     try {
       dispatch(ShowLoading());
       const response = await LoginUser(values);
-
       dispatch(HideLoading());
       if (response.success) {
         message.success(response.message);
@@ -27,10 +22,7 @@ function Login() {
         message.error(response.message);
       }
     } catch (error) {
-
-      // add dispatch
       dispatch(HideLoading());
-
       message.error(error.message);
     }
   };
@@ -42,59 +34,47 @@ function Login() {
     }
   }, []);
 
-
   return (
-    <div className='h-screen bg-primary flex item-center justify-center'>
-      <div className='authentication-form bg-white p-3 rounded'>
-        <h1 className="text-secondary text-2xl">
-          Library - Login
+    <div className="h-screen bg-primary flex items-center justify-center">
+      <div className="authentication-form bg-white p-3 rounded">
+        <h1 className="text-secondary text-2xl font-bold mb-1">
+          SHEYLIBRAY - LOGIN
         </h1>
         <hr />
-        <Form layout="vertical"
-          onFinish={onFinish}
-          className='mt-1 p-3'>
+        <Form layout="vertical" onFinish={onFinish}>
           <Form.Item
             label="Email"
             name="email"
-            rules = {[
-              {
-                required :true,
-                message: "Please input your email",
-              },
-            ]}
-            >
-            <Input type="email" placeholder="Email" />
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-
-            rules ={[
+            rules={[
               {
                 required: true,
-                message :" Please input your password !",
+                message: "Please input your email!",
               },
             ]}
           >
-            <Input type="password" placeholder="Password" />
-
+            <input type="email" placeholder="Email" />
           </Form.Item>
-          <Button title="Login"
-            type="submit" color="secondary" varient="outlined" />
+          <Form.Item label="Password" name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <input type="password" placeholder="Password" />
+          </Form.Item>
+
           <div className="text-center mt-2 flex flex-col gap-1">
             <Button title="Login" type="submit" />
-            <Link to="/register"
-              className="text-primary text-sm underline">Don't Have an account ? click Here to register</Link>
+            <Link to="/register" className="text-primary text-sm underline">
+              Dont have an account? Click Here To Register
+            </Link>
           </div>
-
         </Form>
-
-
-
       </div>
     </div>
-
-  )
+  );
 }
-export default Login
+
+export default Login;
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,27 +1,23 @@
-import React, { useEffect } from 'react';
-import { Button, Form, message } from "antd";
-import Button from '../../components/Button';
-import { Link, useNavigate } from 'react-router-dom';
-import { RegisterUser } from '../../apicalls/users';
-import { useDispatch } from 'react-redux';
-import { HideLoading, ShowLoading } from '../../redux/loadersSlice';
+import React, { useEffect } from "react";
+import { Form, message } from "antd";
+import Button from "../../components/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { RegisterUser } from "../../apicalls/users";
+import { useDispatch } from "react-redux";
+import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
 
 function Register() {
   const navigate = useNavigate();
-
   const dispatch = useDispatch();
-  
-  const onFinish = async(values) => {
+  const onFinish = async (values) => {
     try {
       dispatch(ShowLoading());
       const response = await RegisterUser(values);
-
       dispatch(HideLoading());
-      if(response.success) {
+      if (response.success) {
         message.success(response.message);
-
         navigate("/login");
-      }else {
+      } else {
         message.error(response.message);
       }
     } catch (error) {
@@ -29,93 +25,81 @@ function Register() {
       message.error(error.message);
     }
   };
+
   useEffect(() => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        navigate("/");
-      }
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/");
+    }
   }, []);
+
   return (
-    <div className='h-screen bg-primary flex item-center justify-center'>
-      <div className='authentication-form bg-white p-3 rounded'>
-        <h1 className="text-secondary text-2xl">
-          Library-Register
+    <div className="h-screen bg-primary flex items-center justify-center">
+      <div className="authentication-form bg-white p-3 rounded">
+        <h1 className="text-secondary text-2xl font-bold mb-1">
+          SHEYLIBRAY - REGISTER
         </h1>
         <hr />
-        <Form layout="vertical"
-          onFinish={onFinish}
-          className='mt-1 p-3'>
+        <Form layout="vertical" onFinish={onFinish}>
           <Form.Item
             label="Name"
             name="name"
-            rules ={[
+            rules={[
               {
-                required:true,
+                required: true,
                 message: "Please input your name!",
               },
             ]}
           >
-            <input type="text" placeholder= "Name" />
+            <input type="text" placeholder="Name" />
           </Form.Item>
           <Form.Item
             label="Email"
             name="email"
-
-            rules ={[
+            rules={[
               {
                 required: true,
-                message :"Please input your email!",
+                message: "Please input your email!",
               },
             ]}
-
-            >
-            <Input type="email" placeholder="Email" />
+          >
+            <input type="email" placeholder="Email" />
           </Form.Item>
-
           <Form.Item
-            label="phone Number"
+            label="Phone Number"
             name="phone"
-              
-            rules ={[
-
+            rules={[
               {
-                required:true,
-                message:"Please input your phone number!",
+                required: true,
+                message: "Please input your phone number!",
               },
             ]}
-            >
-            <Input type="number" placeholder="phone Number" />
+          >
+            <input type="number" placeholder="Phone Number" />
           </Form.Item>
-
-
           <Form.Item
             label="Password"
             name="password"
-
             rules={[
               {
-                required:true,
-                message:"Please input your password!",
-              }
+                required: true,
+                message: "Please input your password!",
+              },
             ]}
           >
-            <Input type="password" placeholder="Password" />
-
+            <input type="password" placeholder="Password" />
           </Form.Item>
+
           <div className="text-center mt-2 flex flex-col gap-1">
-
-            <Button title="Register"
-              type="submit" color="secondary" variant ="outlined" />
-
-            <Link to="/login"
-              className="text-primary text-sm underline">Already Have an account ? click Here to Login</Link>
+            <Button title="Register" type="submit" />
+            <Link to="/login" className="text-primary text-sm underline">
+              Already have an account? Click Here To Login
+            </Link>
           </div>
-
         </Form>
-
       </div>
     </div>
-
-  )
+  );
 }
-export default Register
+
+export default Register;
