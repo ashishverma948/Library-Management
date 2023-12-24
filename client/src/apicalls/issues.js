619d72f8a3efce3c2849d7a7a99ffb9c5d16fd8d
priyanshupg <gpriyanshu752@gmail.com>
2023-12-24 12:25:21 +0530

    create issue.js and issue routes.js

--- client/src/apicalls/issues.js
+++ client/src/apicalls/issues.js
@@ -9,3 +9,25 @@ export const IssueBook = async (payload) => {
       throw error;
     }
   }
+
+  //get issues
+  export const GetIssues = async (payload) => {
+    try {
+      const response = await axiosInstance.post("/api/issues/get-issues" , payload);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+}
+
+// return a book
+export const ReturnBook = async (payload) => {
+    try {
+      const response = await axiosInstance.post("/api/issues/return-book", payload);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+}
+
+
--- server/routes/issuesRoute.js
+++ server/routes/issuesRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const issue = require('../models/issuesModel');
 const Book = require('../models/booksModel');
 const authMiddleware = require('../middlewares/authMiddleware');
+const { message } = require('antd');
 
 //issue a book to patron 
 
@@ -29,6 +30,59 @@ try{
 });
 
 
+// get issues
+router.post("/get-issues", authMiddleware, async (req, res) => {
+    try {
+      delete req.body.userIdFromToken;
+      const issues = await Issue.find(req.body).populate("book").populate("user").sort({ issueDate: -1 });
+      return res.send({
+        success: true,
+        message: "Issues fetched successfully",
+        data: issues,
+      });
+    } catch (error) {
+      return res.send({
+        success: false,
+        message: error.message,
+      });
+    }
+  });
+
+//   return a book
+
+router.post("/return-book",authMiddleware,async(req,res)=>{
+    try{
+        //inventory adjustment( avaliable copies must be incremented by 1
+        await Book.findOneAndUpdate(
+            {
+                _id: req.body.book,
+            },
+            {
+                $inc :{availableCopies :1},
+            }
+        
+        );
+
+        //return book (update issue record)
+
+        await Issue.findOneAndUpdate(
+            {
+                _id : req.body._id,
+            },
+            req.body
+        );
+
+        return res.send({
+            success:true,
+            message : "Book returned successfully",
+        });
+    } catch(error){
+        return res.send({
+            success:false,
+            message: error.message,
+        });
+    }
+});
 
 
 module.exports = router;
